aba217d5693d80242d3401f2595b1ba5318957a5
Vinicius Moura <vmoura56@gmail.com>
2020-05-30 20:34:21 -0300

    added validations and tested all models on console, seeded categories with plastic, glass, and paper

--- app/models/accepted_offer.rb
+++ app/models/accepted_offer.rb
@@ -1,4 +1,5 @@
 class AcceptedOffer < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+  validates :user, :offer, presence: true
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,4 +2,5 @@ class Offer < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :acceptedoffers
+  validates :user, :category, :exact_location, :general_location, :volume, :pick_up_on, presence: true
 end
