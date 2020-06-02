69e0d94c9003f6c7636069df3ecea3c33afb09fa
Sophie Evans <emailsophiep@gmail.com>
2020-06-02 15:00:08 -0300

    adding home page index method and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :index, :show]
-
   def home
+  end
+
+  def index
+    @offers = Offer.all
   end
 end
