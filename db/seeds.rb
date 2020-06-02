4448f3f3fba96f306847e97583225a87a16a41d3
Sophie Evans <emailsophiep@gmail.com>
2020-06-02 16:36:31 -0300

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create(name:"Plastic")
+Category.create(name:"Glass")
+Category.create(name:"Paper")
+
+
+Offer.create(volume: "20 plastic bottles", general_location: "London", exact_location: "77 Some Street",
+ pick_up_on: "3 July 2020", category_id: 2, user_id: 1)
+
+Offer.create(volume: "10 glass bottles", general_location: "Hamilton", exact_location: "2 Another Street",
+ pick_up_on: "15 July 2020", category_id: 3, user_id: 1)
+
+puts "seeds created"
