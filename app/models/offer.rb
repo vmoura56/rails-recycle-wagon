9ee4b869f486867dff4d463995ab2408d6bb20bf
Vinicius Moura <vmoura56@gmail.com>
2020-06-02 02:56:05 -0300

    fixed seed bugs

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
   belongs_to :category
   belongs_to :user
-  has_many :accepted_offers
+  has_many :accepted_offers, dependent: :destroy
   validates :user, :category, :exact_location, :general_location, :volume, :pick_up_on, presence: true
   geocoded_by :exact_location
   after_validation :geocode, if: :will_save_change_to_exact_location?
--- db/seeds.rb
+++ db/seeds.rb
@@ -32,57 +32,57 @@ healer = User.create!(email:"healer@recycle.com", password:"123456", username: "
 
 
 Offer.create!(volume: "10 glass bottles", general_location: "Greenwich", exact_location: "7 Bellot Street, London, SE10",
- pick_up_on: "22 June 2020", category: "Glass", user: vinny)
+ pick_up_on: "22 June 2020", category: glass, user: vinny)
 
 Offer.create!(volume: "1 tonne of paper", general_location: "Kensington", exact_location: "12 Kensington High Street, London, W8",
- pick_up_on: "15 July 2020", category: "Paper", user: gabriel)
+ pick_up_on: "15 July 2020", category: paper, user: gabriel)
 
 Offer.create!(volume: "20 plastic bottles", general_location: "Shoreditch", exact_location: "1 Brick Lane, London, E1",
- pick_up_on: "3 July 2020", category: "Plastic", user: sophie)
+ pick_up_on: "3 July 2020", category: plastic, user: sophie)
 
 Offer.create!(volume: "12 cardboard boxes", general_location: "Barbican", exact_location: "22 Old Street, London, EC1V",
- pick_up_on: "17 June 2020", category: "Paper", user: motherearth)
+ pick_up_on: "17 June 2020", category: paper, user: motherearth)
 
 Offer.create!(volume: "77 wine bottles", general_location: "Deptford", exact_location: "22 Deptford High Street, London, SE8",
- pick_up_on: "22 June 2020", category: "Glass", user: recycler)
+ pick_up_on: "22 June 2020", category: glass, user: recycler)
 
 Offer.create!(volume: "1 tonne plastic mixed", general_location: "Clapham", exact_location: "32 The Pavement, London, SW4",
- pick_up_on: "1 August 2020", category: "Plastic", user: planetsaver)
+ pick_up_on: "1 August 2020", category: plastic, user: planetsaver)
 
 Offer.create!(volume: "5 boxes of office paper", general_location: "Holborn", exact_location: "16 Elm Street, London, WC1X",
- pick_up_on: "18 July 2020", category: "Paper", user: healer)
+ pick_up_on: "18 July 2020", category: paper, user: healer)
 
-Offer.create!(volume: "8 sheets of glass 1m x 1m", general_location: "Finsbury Park", exact_location: "3 Finsbury Park Road, London, N4"
- pick_up_on: "4 July 2020", category: "Glass", user: sophie)
+Offer.create!(volume: "8 sheets of glass 1m x 1m", general_location: "Finsbury Park", exact_location: "3 Finsbury Park Road, London, N4",
+ pick_up_on: "4 July 2020", category: glass, user: sophie)
 
-Offer.create!(volume: "8 sheets of glass 1m x 1m", general_location: "Aldwych", exact_location: "66 Kingsway, London, SW14"
- pick_up_on: "24 July 2020", category: "Glass", user: vinny)
+Offer.create!(volume: "8 sheets of glass 1m x 1m", general_location: "Aldwych", exact_location: "66 Kingsway, London, SW14",
+ pick_up_on: "24 July 2020", category: glass, user: vinny)
 
-Offer.create!(volume: "1/2 tonne plastic", general_location: "Vassal", exact_location: "6 Gosling Way, London, SW9"
- pick_up_on: "19 June 2020", category: "Plastic", user: healer)
+Offer.create!(volume: "1/2 tonne plastic", general_location: "Vassal", exact_location: "6 Gosling Way, London, SW9",
+ pick_up_on: "19 June 2020", category: plastic, user: healer)
 
 Offer.create!(volume: "35 cardboard boxes", general_location: "Shoreditch", exact_location: "1 Curtain Road, London, E1",
- pick_up_on: "18 July 2020", category: "Paper", user: gabriel)
+ pick_up_on: "18 July 2020", category: paper, user: gabriel)
 
 Offer.create!(volume: "1/2 tonne glass", general_location: "Shoreditch", exact_location: "10 Worship Street, London, E1",
- pick_up_on: "22 July 2020", category: "Glass", user: motherearth)
+ pick_up_on: "22 July 2020", category: glass, user: motherearth)
 
 Offer.create!(volume: "6 boxes of plastic bags", general_location: "Shoreditch", exact_location: "1 Brick Lane, London, E1",
- pick_up_on: "3 July 2020", category: "Plastic", user: sophie)
+ pick_up_on: "3 July 2020", category: plastic, user: sophie)
 
 Offer.create!(volume: "12 glass bottles", general_location: "Shoreditch", exact_location: "2 Hearn Street, London, EC2A",
- pick_up_on: "1 August 2020", category: "Glass", user: planetsaver)
+ pick_up_on: "1 August 2020", category: glass, user: planetsaver)
 
 Offer.create!(volume: "17 cardboard boxes", general_location: "Shoreditch", exact_location: "1 Brick Lane, London, E1",
- pick_up_on: "3 July 2020", category: "Paper", user: sophie)
+ pick_up_on: "3 July 2020", category: paper, user: sophie)
 
 Offer.create!(volume: "14 boxes of sheet paper", general_location: "Holborn", exact_location: "16 Elm Street, London, WC1X",
- pick_up_on: "18 August 2020", category: "Paper", user: healer)
+ pick_up_on: "18 August 2020", category: paper, user: healer)
 
-Offer.create!(volume: "5 tonnes plastic containers", general_location: "Deptford", exact_location: "6 Gosling Way, London, SW9"
- pick_up_on: "22 June 2020", category: "Plastic", user: recycler)
+Offer.create!(volume: "5 tonnes plastic containers", general_location: "Deptford", exact_location: "6 Gosling Way, London, SW9",
+ pick_up_on: "22 June 2020", category: plastic, user: recycler)
 
 Offer.create!(volume: "27 plastic bottles", general_location: "Finsbury Park", exact_location: "3 Finsbury Park Road, London, N4",
- pick_up_on: "18 July 2020", category: "Plastic", user: sophie)
+ pick_up_on: "18 July 2020", category: plastic, user: sophie)
 
 puts "seeds created"
