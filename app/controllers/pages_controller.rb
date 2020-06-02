0b2e0e881ea150a0efc6e86ea47e479a460e2b49
Vinicius Moura <vmoura56@gmail.com>
2020-06-02 02:08:50 -0300

    integrated location filter with search and accepted offers filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :index, :show]
 
   def home
-    @offers = Offer.where.not(id: AcceptedOffer.select(:offer_id).uniq)
-
     client_ip = request.remote_ip
     # If testing from local host, ip will set to a known London ip
     if client_ip == "::1"
@@ -12,7 +10,7 @@ class PagesController < ApplicationController
 
     @user_location = Geocoder.search(client_ip).first.coordinates
   
-    @offers_near = Offer.joins(:category).geocoded.near(@user_location, 100)
+    @offers_near = Offer.where.not(id: AcceptedOffer.select(:offer_id).uniq).geocoded.near(@user_location, 100)
 
     @markers = @offers_near.map do |offer|
       {
